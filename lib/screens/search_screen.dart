dd16baedefc26c249ff8e144f934f2a5012eb727
MichaellIbarra <michaell.ibarra@vallegrande.edu.pe>
2024-10-16 02:11:53 +0000

    a

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -14,7 +14,8 @@ class SearchScreen extends StatefulWidget {
   State<SearchScreen> createState() => _SearchScreenState();
 }
 
-class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderStateMixin {
+class _SearchScreenState extends State<SearchScreen>
+    with SingleTickerProviderStateMixin {
   late TextEditingController searchTextController;
   List<ProductModel> productList = [];
   List<ProductModel> productListSearch = [];
@@ -50,7 +51,8 @@ class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderSt
 
   Future<void> _fetchInactiveProducts() async {
     try {
-      List<ProductModel> products = await ProductService.fetchInactiveProducts();
+      List<ProductModel> products =
+          await ProductService.fetchInactiveProducts();
       setState(() {
         inactiveProductList = products;
         inactiveProductListSearch = products; // Initialize search list
@@ -72,9 +74,11 @@ class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderSt
   }
 
   void _searchProducts(String query, bool isActive) {
-    List<ProductModel> filteredList = isActive ? productList : inactiveProductList;
+    List<ProductModel> filteredList =
+        isActive ? productList : inactiveProductList;
     List<ProductModel> searchList = filteredList
-        .where((product) => product.name.toLowerCase().contains(query.toLowerCase()))
+        .where((product) =>
+            product.name.toLowerCase().contains(query.toLowerCase()))
         .toList();
 
     setState(() {
@@ -87,8 +91,10 @@ class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderSt
   }
 
   Widget _buildProductList(bool isActive) {
-    List<ProductModel> filteredProductList = isActive ? productList : inactiveProductList;
-    List<ProductModel> productListSearch = isActive ? this.productListSearch : inactiveProductListSearch;
+    List<ProductModel> filteredProductList =
+        isActive ? productList : inactiveProductList;
+    List<ProductModel> productListSearch =
+        isActive ? this.productListSearch : inactiveProductListSearch;
 
     return isLoading
         ? const Center(child: CircularProgressIndicator())
@@ -108,7 +114,8 @@ class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderSt
                           onTap: () {
                             FocusScope.of(context).unfocus();
                             searchTextController.clear();
-                            _searchProducts('', isActive); // Clear search results
+                            _searchProducts(
+                                '', isActive); // Clear search results
                           },
                           child: const Icon(Icons.clear, color: Colors.red),
                         ),
@@ -118,19 +125,26 @@ class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderSt
                       },
                     ),
                     const SizedBox(height: 15.0),
-                    if (searchTextController.text.isNotEmpty && productListSearch.isEmpty)
-                      const Center(child: TitlesTextWidget(label: "No products found")),
+                    if (searchTextController.text.isNotEmpty &&
+                        productListSearch.isEmpty)
+                      const Center(
+                          child: TitlesTextWidget(label: "No products found")),
                     Expanded(
                       child: GridView.builder(
-                        itemCount: searchTextController.text.isNotEmpty ? productListSearch.length : filteredProductList.length,
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        itemCount: searchTextController.text.isNotEmpty
+                            ? productListSearch.length
+                            : filteredProductList.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 2,
                           mainAxisSpacing: 12,
                           crossAxisSpacing: 12,
                           childAspectRatio: 0.75,
                         ),
                         itemBuilder: (context, index) {
-                          final product = searchTextController.text.isNotEmpty ? productListSearch[index] : filteredProductList[index];
+                          final product = searchTextController.text.isNotEmpty
+                              ? productListSearch[index]
+                              : filteredProductList[index];
                           return ProductWidget(
                             product: product,
                             isActive: isActive,
@@ -138,7 +152,9 @@ class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderSt
                               await Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => EditOrUploadProductScreen(productModel: product),
+                                  builder: (context) =>
+                                      EditOrUploadProductScreen(
+                                          productModel: product),
                                 ),
                               );
                               // Refetch products after returning from edit screen
@@ -166,7 +182,8 @@ class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderSt
 
   @override
   Widget build(BuildContext context) {
-    String? passedCategory = ModalRoute.of(context)!.settings.arguments as String?;
+    String? passedCategory =
+        ModalRoute.of(context)!.settings.arguments as String?;
 
     return GestureDetector(
       onTap: () {
@@ -206,7 +223,8 @@ class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderSt
             visible: _tabController.index == 0,
             child: FloatingActionButton(
               onPressed: () {
-                Navigator.pushNamed(context, EditOrUploadProductScreen.routeName);
+                Navigator.pushNamed(
+                    context, EditOrUploadProductScreen.routeName);
               },
               child: const Icon(Icons.add),
             ),
