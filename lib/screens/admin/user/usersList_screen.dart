83ff1c22a437f8c119506dbef27cd6702d30eac4
MichaellIbarra <michaell.ibarra@vallegrande.edu.pe>
2024-10-17 00:38:38 +0000

    a

--- lib/screens/admin/user/usersList_screen.dart
+++ lib/screens/admin/user/usersList_screen.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 
 import 'package:myapp/screens/admin/user/editarPage_screen.dart';
 import 'package:myapp/services/assets_manager.dart';
+import 'package:myapp/widgets/title_text.dart';
 
 class UserslistScreen extends StatefulWidget {
   static const routeName = '/UserslistScreen';
@@ -17,6 +18,7 @@ class _UserslistScreenState extends State<UserslistScreen>
     with SingleTickerProviderStateMixin {
   late TextEditingController searchTextController;
   List<dynamic> users = [];
+  List<dynamic> allUsers = [];
   bool isActive = true;
   bool isLoading = true;
   late TabController _tabController;
@@ -36,7 +38,8 @@ class _UserslistScreenState extends State<UserslistScreen>
 
     if (response.statusCode == 200) {
       setState(() {
-        users = json.decode(response.body);
+        allUsers = json.decode(response.body);
+        users = allUsers;
         isLoading = false;
       });
     } else {
@@ -63,132 +66,138 @@ class _UserslistScreenState extends State<UserslistScreen>
   }
 
   void _searchUsers(String query) {
-    List<dynamic> filteredList = users.where((user) {
-      final fullName = '${user['first_name']} ${user['last_name']}';
-      return fullName.toLowerCase().contains(query.toLowerCase());
-    }).toList();
+    if (query.isEmpty) {
+      setState(() {
+        users = allUsers;
+      });
+    } else {
+      List<dynamic> filteredList = allUsers.where((user) {
+        final fullName = '${user['first_name']} ${user['last_name']}';
+        return fullName.toLowerCase().contains(query.toLowerCase());
+      }).toList();
 
-    setState(() {
-      users = filteredList;
-    });
+      setState(() {
+        users = filteredList;
+      });
+    }
   }
 
   Widget _buildUserList() {
     return isLoading
         ? const Center(child: CircularProgressIndicator())
-        : users.isEmpty
-            ? const Center(child: Text("Usuarios no encontrados"))
-            : Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 15.0),
-                    TextField(
-                      controller: searchTextController,
-                      decoration: InputDecoration(
-                        hintText: "Buscar",
-                        prefixIcon: const Icon(Icons.search),
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            FocusScope.of(context).unfocus();
-                            searchTextController.clear();
-                            _searchUsers(''); // Clear search results
-                          },
-                          child: const Icon(Icons.clear, color: Colors.red),
-                        ),
-                      ),
-                      onChanged: (value) {
-                        _searchUsers(value);
+        : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 15.0),
+                TextField(
+                  controller: searchTextController,
+                  decoration: InputDecoration(
+                    hintText: "Buscar",
+                    prefixIcon: const Icon(Icons.search),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        FocusScope.of(context).unfocus();
+                        searchTextController.clear();
+                        _searchUsers(''); // Clear search results
                       },
+                      child: const Icon(Icons.clear, color: Colors.red),
                     ),
-                    const SizedBox(height: 15.0),
-                    if (searchTextController.text.isNotEmpty && users.isEmpty)
-                      const Center(child: Text("No usuarios found")),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: users.length,
-                        itemBuilder: (context, index) {
-                          final user = users[index];
-                          return Card(
-                            elevation: 5,
-                            margin: const EdgeInsets.all(5),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: 50.0,
-                                    height: 50.0,
-                                    child: Image.asset(AssetsManager.shoppingCart),
-                                  ),
-                                  const SizedBox(width: 15.0),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          '${user['first_name']} ${user['last_name']}',
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        Text(
-                                            'Documento: ${user['document_type']} ${user['document_number']}'),
-                                        Text('Email: ${user['email']}'),
-                                        Text('Teléfono: ${user['phone']}'),
-                                        Text('Dirección: ${user['address']}'),
-                                      ],
+                  ),
+                  onChanged: (value) {
+                    _searchUsers(value);
+                  },
+                ),
+                const SizedBox(height: 15.0),
+                if (searchTextController.text.isNotEmpty && users.isEmpty)
+                  const Center(child: Text("No usuarios found")),
+                Expanded(
+                  child: users.isEmpty
+                      ? const Center(child: Text("Usuarios no encontrados"))
+                      : ListView.builder(
+                          itemCount: users.length,
+                          itemBuilder: (context, index) {
+                            final user = users[index];
+                            return Card(
+                              elevation: 5,
+                              margin: const EdgeInsets.all(5),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      width: 50.0,
+                                      height: 50.0,
+                                      child: Image.asset(AssetsManager.shoppingCart),
                                     ),
-                                  ),
-                                  Column(
-                                    children: [
-                                      if (isActive)
-                                        IconButton(
-                                          icon: const Icon(Icons.edit),
-                                          onPressed: () {
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                builder: (context) =>
-                                                    EditUserPage(
-                                                  userId: user['id_user']
-                                                      .toString(),
-                                                  userData: user,
+                                    const SizedBox(width: 15.0),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            '${user['first_name']} ${user['last_name']}',
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Text(
+                                              'Documento: ${user['document_type']} ${user['document_number']}'),
+                                          Text('Email: ${user['email']}'),
+                                          Text('Teléfono: ${user['phone']}'),
+                                          Text('Dirección: ${user['address']}'),
+                                        ],
+                                      ),
+                                    ),
+                                    Column(
+                                      children: [
+                                        if (isActive)
+                                          IconButton(
+                                            icon: const Icon(Icons.edit),
+                                            onPressed: () {
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      EditUserPage(
+                                                    userId: user['id_user']
+                                                        .toString(),
+                                                    userData: user,
+                                                  ),
                                                 ),
-                                              ),
-                                            ).then((value) {
-                                              if (value == true) {
-                                                fetchUsers();
-                                              }
-                                            });
+                                              ).then((value) {
+                                                if (value == true) {
+                                                  fetchUsers();
+                                                }
+                                              });
+                                            },
+                                          ),
+                                        IconButton(
+                                          icon: Icon(isActive
+                                              ? Icons.delete
+                                              : Icons.restore),
+                                          onPressed: () {
+                                            eliminarPersona(
+                                                user['id_user'].toString(),
+                                                isActive);
                                           },
                                         ),
-                                      IconButton(
-                                        icon: Icon(isActive
-                                            ? Icons.delete
-                                            : Icons.restore),
-                                        onPressed: () {
-                                          eliminarPersona(
-                                              user['id_user'].toString(),
-                                              isActive);
-                                        },
-                                      ),
-                                    ],
-                                  )
-                                ],
+                                      ],
+                                    )
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
+                            );
+                          },
+                        ),
                 ),
-              );
+              ],
+            ),
+          );
   }
 
   @override
@@ -201,8 +210,7 @@ class _UserslistScreenState extends State<UserslistScreen>
         length: 2,
         child: Scaffold(
           appBar: AppBar(
-            title: Text(
-                "Lista de Usuarios ${isActive ? 'Activos' : 'Inactivos'}"),
+            title: const TitlesTextWidget(label: "Usuarios"),
             bottom: TabBar(
               controller: _tabController,
               tabs: const [
@@ -277,7 +285,7 @@ class _UserslistScreenState extends State<UserslistScreen>
     if (confirmAction == true) {
       try {
         final response = await http.put(Uri.parse(
-            'https://psychic-capybara-4xvq5g9x97j377v5-8085.app.github.dev/api/v1/users/$id/${estado ? 'inactivar' : 'restaurar'}'));
+            'https://psychic-capybara-4vq5g9x97j377v5-8085.app.github.dev/api/v1/users/$id/${estado ? 'inactivar' : 'restaurar'}'));
 
         setState(() {
           users.removeWhere((user) => user['id_user'].toString() == id);
